dec6c26e5a2d4c44968b8786210013ed0aee937d
pharmakarios1 <akhigbeblessed@gmail.com>
2026-02-10 22:25:51 +0100

    fix: unused state hook

--- src/components/Hero/_partials/HeroTexts/index.tsx
+++ src/components/Hero/_partials/HeroTexts/index.tsx
@@ -3,11 +3,11 @@ import PhoneField from "@components/PhoneInput";
 const HeroTexts = () => {
   return (
     <div className=" ">
-      <p className="font-bold text-[46px] text-[#444C66]">
+      <p className="font-bold text-center md:text-start text-[36px] md:text-[46px] text-[#444C66]">
         Earn more <span className="text-[#FA7B20]">money</span> <br /> when you{" "}
         <span className="text-blue-700"> pay bills</span>{" "}
       </p>
-      <p className="my-10">
+      <p className="my-10 text-center md:text-start ">
         Scaripay provides you with a simple & smart way to earn <br /> residual
         Income with just your mobile (phone) number
       </p>
