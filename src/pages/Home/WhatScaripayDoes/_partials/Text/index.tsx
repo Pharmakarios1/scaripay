f2638b3068dec8c8ddb830d41c7b0e0e30a70cc4
pharmakarios1 <akhigbeblessed@gmail.com>
2026-02-14 11:30:00 +0100

    overhaul whatScaripay does component

--- src/pages/Home/WhatScaripayDoes/_partials/Text/index.tsx
+++ src/pages/Home/WhatScaripayDoes/_partials/Text/index.tsx
@@ -4,12 +4,12 @@ const WhatScaripayDoesText = ({}) => {
       <h2 className="text-[20px] md:text-[32px] text-[#444C66] font-bold mb-10">
         What can you <br /> do with Scaripay?
       </h2>
-      <div className="shadow-md w-full md:max-w-150 max-h-53 p-8 md:p-15 border border-gray-100 rounded-md">
-        <h4 className="font-semibold text-orange-400">
+      <div className="shadow-md w-full md:max-w-150 max-h-53 p-8 md:p-15  border border-gray-100 rounded-md">
+        <h4 className="font-semibold text-orange-400 py-5">
           {" "}
           Airtime & Data Subscription
         </h4>
-        <p>
+        <p className="text-sm md:text-base">
           Send Airtime & Internet Data directly to anyone on their phone number
           via Scaripay, and you earn commission & Incentives in real time to
           your Scaripay wallet.
