3d00db1093ca63c53255cb96eea00a95dc2b7472
pharmakarios1 <akhigbeblessed@gmail.com>
2026-02-24 17:35:31 +0100

    add cards to hold each functionality

--- src/pages/Dashboard/DashboardHome/index.tsx
+++ src/pages/Dashboard/DashboardHome/index.tsx
@@ -1,17 +1,15 @@
 import CustomButton from "@components/Button";
-import { Card, Row, Col, Divider} from "antd";
-
+import { Card, Row, Col, Divider } from "antd";
 
 //images
 import GraphOnEmtystate from "@assets/graph.png";
-import pointValue from "@assets/point value.png"
-import commission from "@assets/commision icon.png"
-import GiftIcon from "@assets/Gift Icon.png"
+import pointValue from "@assets/point value.png";
+import commission from "@assets/commision icon.png";
+import GiftIcon from "@assets/Gift Icon.png";
 
 const DashboardHome = () => {
-
   return (
-    <div className="">
+    <div className="space-y-10">
       <Row gutter={[16, 16]}>
         <Col xs={24} md={12}>
           <Card className="h-35! bg-[#FFF8F4]!">
@@ -24,7 +22,10 @@ const DashboardHome = () => {
                 <div className="flex gap-3 items-center">
                   <p className="text-xl">Ar</p>
                   <p className="text-gray-600">
-                    <span className="text-2xl sm:text-5xl font-semibold">0</span>: 00
+                    <span className="text-2xl sm:text-5xl font-semibold">
+                      0
+                    </span>
+                    : 00
                   </p>
                 </div>
               </div>
@@ -64,41 +65,51 @@ const DashboardHome = () => {
           <Card className="h-[170px]!">
             <div className="h-[60px] flex items-center gap-5">
               <div className="h-8 w-8 rounded-md bg-[#CD5805]/40 flex justify-center items-center">
-              <img src={pointValue} className="w-5 h-5"/>
+                <img src={pointValue} className="w-5 h-5" />
               </div>
               <p className="text-[#757A89]">Total Point Value</p>
             </div>
             <p className="text-gray-600">
-                    <span className="text-2xl sm:text-5xl font-semibold">0</span>
-                  </p>
+              <span className="text-2xl sm:text-5xl font-semibold">0</span>
+            </p>
           </Card>
         </Col>
 
         <Col xs={24} md={8}>
-           <Card className="h-[170px]!">
+          <Card className="h-[170px]!">
             <div className="h-[60px] flex items-center gap-5">
               <div className="h-8 w-8 rounded-md bg-[#6EE7B7]/40 flex justify-center items-center">
-              <img src={commission} className="w-5 h-5"/>
+                <img src={commission} className="w-5 h-5" />
               </div>
               <p className="text-[#757A89]">Total Point Value</p>
             </div>
             <div className="flex gap-3 items-center">
-                  <p className="text-xl">Ar</p>
-                  <p className="text-gray-600">
-                    <span className="text-2xl sm:text-5xl font-semibold">0</span>: 00
-                  </p>
-                </div>
+              <p className="text-xl">Ar</p>
+              <p className="text-gray-600">
+                <span className="text-2xl sm:text-5xl font-semibold">0</span>:
+                00
+              </p>
+            </div>
           </Card>
         </Col>
 
         <Col xs={24} md={8}>
           <Card className="bg-[#173AE5]! h-[170px]!">
             <div className="flex items-center justify-between">
-              <p className="text-white text-base lg:text-[20px] font-semibold">Refer a Friend 
-and Earn</p>
-<img src={GiftIcon} className=""/>
+              <p className="text-white text-base lg:text-[20px] font-semibold">
+                Refer a Friend and Earn
+              </p>
+              <img src={GiftIcon} className="" />
             </div>
           </Card>
+        </Col>
+      </Row>
+      <Row gutter={[16, 16]}>
+        <Col xs={24} md={12}>
+          <Card className="h-35! bg-[#FFF8F4]!">recent acitvities</Card>
+        </Col>
+        <Col xs={24} md={12}>
+          <Card className="h-35! bg-[#FFF8F4]!">latest transactions</Card>
         </Col>
       </Row>
     </div>
